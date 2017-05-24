cce094a2c4045f5b2d2c442ca7203c6dbbfbc80c
David <greenzeal@Davids-MacBook-Pro.local>
2017-05-24 12:13:04 +0400

    Initial Commit

---
+++ QRReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRReader
+//
+//  Created by David on 5/24/17.
+//  Copyright © 2017 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRReader
+//
+//  Created by David on 5/24/17.
+//  Copyright © 2017 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
